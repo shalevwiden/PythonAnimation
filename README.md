64444255aefdf9a31aefa18db25e6a6c84ba2ad5
Shalev Widen <shalevwiden@gmail.com>
2025-09-05 02:25:27 -0500

    Added helper functions. Big progess

--- README.md
+++ README.md
@@ -8,4 +8,18 @@ I've posted videos I've made from this project at -insert link-.
 This project was a huge learning experience. With challenge after challenge, making me realize I needed more specific code. Learned lamda alot better to sort the list of images so they would open in order.
 I also created a map degrees function that would simulate going around a circle with `rotateval` of incrementation and `count` of amount of times.
 
-The pythonanimations.py file contains the bulk of the code. The deleteimages_inimagesfolder.py simply resets the images folder by deleting all images for a new animation. And 360 mapping is where I developed the code to track degree intervals. Its more fleshed out with print statements in that file, versus in pythonanimations.py in simply appends to a list. 
+The pythonanimations.py file contains the bulk of the code. The deleteimages_inimagesfolder.py simply resets the images folder by deleting all images for a new animation. And 360 mapping is where I developed the code to track degree intervals. Its more fleshed out with print statements in that file, versus in pythonanimations.py in simply appends to a list.
+
+## Sep 4, 2025
+
+Creating different colors of this stuff.
+
+I'm also learning the logic for text animations.
+
+```python
+if fullstring:
+        title += fullstring[0]           # take the first character
+        fullstring = fullstring[1:]
+```
+
+Can I change margin beneath the title?
--- pythonanimations.py
+++ pythonanimations.py
@@ -5,6 +5,9 @@ import subprocess
 
 from matplotlib import pyplot as plt
 
+from deleteimages_inimagesfolder import deleteallimagesinimagefolder
+
+
 # do this tmr
 
 # set the amount of colors for the amount of arguments too
@@ -13,10 +16,11 @@ from matplotlib import pyplot as plt
 colors = ['gold', 'skyblue', 'lightcoral', 'mediumseagreen']
 
 
-def animateinpreview(folderpath,delaysecs):
+def animateinsafari(folderpath,delaysecs):
 
     listofitems=os.listdir(folderpath)
-    # sort them. They must be named "chart(num)" for this to work
+    listofitems=[item for item in listofitems if ".ds" not in item.lower()]
+    # sort them to open properly. They must be named "chart(num)" for this to work
     # they are named that later on.
     listofitems=sorted(listofitems,key=lambda x:int(os.path.splitext(x)[0].replace('chart','')))
 
@@ -28,12 +32,11 @@ def animateinpreview(folderpath,delaysecs):
         print(fullpath)
 
         # thats wierd it was opening preview
-        webbrowser.open_new_tab(fullpath)
+        subprocess.run(["open", "-a", "Safari", fullpath])
         time.sleep(delaysecs)
         # close preview 5 seconds after the animation 
     time.sleep(5)
     print(f'The animation concluded and lasted {lengthofanimation}')
-    subprocess.run(['osascript', '-e','quit app "Preview"'])
 
 def animateinchrome(folderpath,delaysecs):
 
@@ -56,7 +59,7 @@ def animateinchrome(folderpath,delaysecs):
 
 
 
-# animateinpreview(graphspath)
+# animateinsafari(graphspath)
 
 def getsymmetricaldata_andcolors(num):
     datasizes=[]
@@ -76,9 +79,10 @@ def getsymmetricaldata_andcolors(num):
     for i in range(num):
         datasizes.append(1/(num))
     return datasizes,colors
-print(getsymmetricaldata_andcolors(11))
 
-def getdynamicdata_andcolors(num):
+# print(getsymmetricaldata_andcolors(11))
+
+def getcolors():
     datasizes=[]
     colors=[
     "#0B1F4B",  # Dark navy blue
@@ -93,15 +97,73 @@ def getdynamicdata_andcolors(num):
     "#D3DD87",  # Pale yellow-green
     "#EFC373",  # Soft warm tan-orange
 ]
+    return colors
+
+def get_similar_colors(colorhex,steps):
+    '''
+    This returns 20 of a color, but starting very dark and getting slightly ligther. 
+    The more steps there are, the more subtle the color change. 
+    '''
+    colorhex = colorhex.lstrip("#")
+
+    # Convert to RGB (0–255)
+    r, g, b = tuple(int(colorhex[i:i+2], 16) for i in (0, 2, 4))
+
+    colorlist = []
+    for i in range(steps):
+        # Factor goes from 1.0 (original) up to 0.0 (white)
+        factor = 1 - (i / steps)
+        new_r = int(r + (255 - r) * (1 - factor))
+        new_g = int(g + (255 - g) * (1 - factor))
+        new_b = int(b + (255 - b) * (1 - factor))
+
+        colorlist.append(f"#{new_r:02X}{new_g:02X}{new_b:02X}")
+
+    return colorlist
+
+def get_alternating_colors(base_color, steps=12, delta=20):
+    """
+    Generate colors cycling every 3:
+    - lighter version of base
+    - darker version of base
+    - the base color itself
+    """
+    base_color = base_color.lstrip("#")
+    r, g, b = tuple(int(base_color[i:i+2], 16) for i in (0, 2, 4))
+
+    color_list = []
+    for i in range(steps):
+        mode = i % 3  # 0=lighter, 1=darker, 2=base
+
+        if mode == 0:  # lighter
+            new_r = min(255, r + delta * (i // 3))
+            new_g = min(255, g + delta * (i // 3))
+            new_b = min(255, b + delta * (i // 3))
+
+        elif mode == 1:  # darker
+            new_r = max(0, r - delta * (i // 3))
+            new_g = max(0, g - delta * (i // 3))
+            new_b = max(0, b - delta * (i // 3))
+
+        else:  # base
+            new_r, new_g, new_b = r, g, b
+
+        color_list.append(f"#{new_r:02X}{new_g:02X}{new_b:02X}")
+
+    return color_list
+
+def get_data(num):
+    datasizes=[]
     for i in range(1,num+1):
-        datasizes.append(1/(i))
-    return datasizes,colors
-    # make data where one datapoint has 1/2, the next 1/3, etc
+        datasizes.append(1/(num))
+    return datasizes
 
 def getfibbonacidata(num):
+    '''
+    I can do this actually with a loop or with recursive
+    '''
     pass
 
-print(getdynamicdata_andcolors(11))
 
 
 
@@ -140,7 +202,10 @@ def makedynamicpiecharts(chartdata, chartcolors,chartcount,pie_initialdegrees,pi
     """
     This makes and saves the images to a folder specified in the function, images folder.
     The function could be changed to take the folder as argument. 
+
+    Rotate val will affect how many degrees each chart changes from one to the next
     """
+
     styles=['fivethirtyeight','ggplot']
     plt.style.use(styles[1])
     imagesfolder='/Users/shalevwiden/Downloads/Coding_Files/Python/BeautifulSoup_Library/python_animation/images'
@@ -152,6 +217,7 @@ def makedynamicpiecharts(chartdata, chartcolors,chartcount,pie_initialdegrees,pi
     # chart is the chartindex
     for chart in range(chartcount):
         
+
         plt.figure(figsize=(figx,figy))
 
         plt.pie(chartdata,colors=chartcolors,startangle=degreeslist[chart])
@@ -161,7 +227,12 @@ def makedynamicpiecharts(chartdata, chartcolors,chartcount,pie_initialdegrees,pi
         if legend:
             plt.legend(loc='lower right',bbox_to_anchor=(1.1, -0.1))
 
-        plt.savefig(f'{imagesfolder}/chart{chart}.png')
+        
+        chartfile=f'chart{chart}.png'
+        savepath=os.path.join(imagesfolder,chartfile)
+        plt.savefig(savepath)
+        subprocess.run(["xattr", "-c", savepath])
+
 
         # make it rounded for formatting
         figx*=(1+scaleval)
@@ -173,8 +244,83 @@ def makedynamicpiecharts(chartdata, chartcolors,chartcount,pie_initialdegrees,pi
         if testing:
             print(f'Saved {imagesfolder}/chart{chart}.png\nWith figx{figx} and figy{figy}\n\
                   and startangle of {degreeslist[chart]}\n')
-    
+            
+    subprocess.run([
+        "xattr",
+        "-cr",
+        "/Users/shalevwiden/Downloads/Coding_Files/Python/BeautifulSoup_Library/python_animation/images"
+    ])
 
+def makesamecolorcharts(chartdata, chartcolors,chartcount,pie_initialdegrees,pie_rotateval=10, scaleval=1.1,legend=True):
+    """
+    This makes and saves the images to a folder specified in the function, images folder.
+    The function could be changed to take the folder as argument. 
+    """
+
+    styles=['fivethirtyeight','ggplot']
+    plt.style.use(styles[1])
+    imagesfolder='/Users/shalevwiden/Downloads/Coding_Files/Python/BeautifulSoup_Library/python_animation/images'
+
+    # this is the data in EACH pie chart. 
+    degreeslist=mapdegrees(initialdegrees=pie_initialdegrees,rotateval=pie_rotateval,count=chartcount)
+
+    sorteddegrees=False
+    if sorteddegrees:
+        degreeslist=sorted(degreeslist)
+    print(f'len:{len(degreeslist)}')
+    figx, figy=9,6
+    # chart is the chartindex
+    fullstring='Hundreds of Slices'
+    # fullstring += ''.join([' 100 ' if i % 2 == 0 else '100' for i in range(chartcount-len(fullstring))])
+    title=""
+    for chart in range(chartcount):
+        
+        plt.close()
+        plt.figure(figsize=(figx,figy))
+        
+        newangle= -(int(degreeslist[chart]))
+        plt.pie(chartdata,colors=chartcolors,startangle=newangle)
+        plt.title(title)
+        
+        title=fullstring
+        plt.title('Alternating Colors')
+
+
+        # breaknum=len(fullstring)
+        # if breaknum>chart+1:
+        #     title+=fullstring[chart]
+        # else:
+        #     title=fullstring[1:]
+
+
+        
+
+        plt.axis('equal')
+
+        if legend:
+            plt.legend(loc='lower right',bbox_to_anchor=(1.1, -0.1))
+
+        chartfile=f'chart{chart}.png'
+        savepath=os.path.join(imagesfolder,chartfile)
+        plt.savefig(savepath)
+
+
+        # make it rounded for formatting
+        figx*=scaleval
+        figx=round(figx,2)
+        figy*=scaleval
+        figy=round(figy,2)
+        
+        testing=True
+        if testing:
+            print(f'Saved {imagesfolder}/chart{chart}.png\nWith figx{figx} and figy{figy}\n\
+                  and startangle of {degreeslist[chart]}\n')
+        
+    subprocess.run([
+        "xattr",
+        "-cr",
+        "/Users/shalevwiden/Downloads/Coding_Files/Python/BeautifulSoup_Library/python_animation/images"
+    ])
 
 
 
@@ -186,17 +332,33 @@ def makebarchart():
     pass
 
 
-def main():
+def makecharts():
 
     print('Making Dynamic Pie Charts now:')
 
-    datalist, colorlist=getdynamicdata_andcolors(11)
-    makedynamicpiecharts(chartdata=datalist,chartcolors=colorlist,chartcount=40,pie_initialdegrees=0,pie_rotateval=30)
-    # animateinpreview(imagesfolder,delaysecs=.5)
+    # the data is whats important for determining the colors
+    sectors=200
+    datalist=get_data(sectors)
+    # colors=get_similar_colors("#5954EE",steps=sectors)
+    colors=get_alternating_colors("#3D3B9A",steps=sectors)
 
-    print(f'Animate in chrome\n {animateinchrome(folderpath='/Users/shalevwiden/Downloads/Coding_Files/Python/BeautifulSoup_Library/python_animation/images',delaysecs=.1)}')
+    # colors=sorted(colors,reverse=True)
+    deleteallimagesinimagefolder(imagefolderpath=imagesfolder)
+
+    makesamecolorcharts(chartdata=datalist,chartcolors=colors,pie_initialdegrees=180,chartcount=7)
+    # makedynamicpiecharts(chartdata=datalist,chartcolors=getcolors(),chartcount=40,pie_initialdegrees=0,pie_rotateval=30)
+    #
+
 
     print(os.getcwd())
-# if __name__=='__main__':
-#     main()
+    time.sleep(2)
 
+makecharts()
+
+def run():
+    
+
+    animateinsafari(imagesfolder,delaysecs=.1)
+    # print(f'Animate in chrome\n {animateinchrome(folderpath='/Users/shalevwiden/Downloads/Coding_Files/Python/BeautifulSoup_Library/python_animation/images',delaysecs=.1)}')
+
+# run()
